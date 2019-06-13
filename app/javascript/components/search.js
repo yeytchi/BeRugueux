bb8e7dd67d70850395a89ce36a546da8f5da881a
nicolas beaudouin <nicolas.pn.beaudouin@gmail.com>
2019-06-13 15:25:35 +0200

    adding searchbar in players index

---
+++ app/javascript/components/search.js
@@ -0,0 +1,17 @@
+const searchPlayer = (query) => {
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        const movie = `<li class="list-inline-item">
+          <img src="${result.Poster}" alt="">
+          <p>${result.Title}</p>
+        </li>`;
+        results.insertAdjacentHTML("beforeend", movie);
+      });
+    });
+};
+
+export { searchPlayer };
+
+

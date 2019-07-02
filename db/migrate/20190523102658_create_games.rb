3da2efe3385aa80a54004534f29b1372e5e7a396
nicolas beaudouin <nicolas.pn.beaudouin@gmail.com>
2019-07-02 10:19:50 +0200

    work in progress

--- db/migrate/20190523102658_create_games.rb
+++ db/migrate/20190523102658_create_games.rb
@@ -2,6 +2,11 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.references :day, foreign_key: true
+      t.references :games, :home_team
+      t.refences :games, :away_team
+      t.boolean :played, default: false
+      t.integer :home_team_score
+      t.integer :away_team_score
     end
   end
 end
